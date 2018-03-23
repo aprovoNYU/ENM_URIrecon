1429be441705d2b82a73375bf9415269776cdf2f
aprovoNYU <alexandra.provo@nyu.edu>
2018-03-23 14:40:05 -0400

    updating URI enrichment scripts and trying with important topics list

--- viafURIsRDF_enm.py
+++ viafURIsRDF_enm.py
@@ -1,113 +1,141 @@
 import requests, json
-import xml.etree.ElementTree as ET
+#import xml.etree.ElementTree as ET
 import rdflib
-from rdflib.namespace import SKOS, RDFS, RDF
+from rdflib import URIRef
 from rdflib import Namespace
-from collections import Counter
+from rdflib.namespace import SKOS, RDFS, RDF
+
+#from collections import Counter
 
 #getting unique items in a list in a fast way, courtesy of https://www.peterbe.com/plog/uniqifiers-benchmark
+#use this when extracting names
+# def f5(seq, idfun=None): 
+#    # order preserving
+#    if idfun is None:
+#        def idfun(x): return x
+#    seen = {}
+#    result = []
+#    for item in seq:
+#        marker = idfun(item)
+#        # in old Python versions:
+#        # if seen.has_key(marker)
+#        # but in new ones:
+#        if marker in seen: continue
+#        seen[marker] = 1
+#        result.append(item)
+#    return result
 
-def f5(seq, idfun=None): 
-   # order preserving
-   if idfun is None:
-       def idfun(x): return x
-   seen = {}
-   result = []
-   for item in seq:
-       marker = idfun(item)
-       # in old Python versions:
-       # if seen.has_key(marker)
-       # but in new ones:
-       if marker in seen: continue
-       seen[marker] = 1
-       result.append(item)
-   return result
-
-
+#specify the schema.org namespace
 schema=Namespace("http://schema.org/")
 schema.name
 
+#create a list for dictionaries containing the enrichment data
 URIdictlist = []
-with open ("Topics_Z.txt") as json_data:
+#open the export from OpenRefine
+with open ("important_topics_to_enrich_2017_12_20_03_14_PM_cleaned-tsv_ORexport_2018_03_23_12-18pm.txt") as json_data:
 	topics = json.load(json_data)
 	for rows in topics:
-		
+		#get to the topic records in the JSON
 		for topic in topics[rows]:
+			#create a dictionary to store key-value pairs that will be turned into JSON later
 			URIdict = {}
-			basketID = topic['basket - id']
-			VIAF_URL = str(topic['VIAF URL'])
-			#print(FAST_URL)
+			#map the basket ID to the terminology used in OpenRefine project
+			basketID = topic["Tct_Id"]
+			#here's the VIAF URI I found through OpenRefine
+			VIAF_URI = topic["VIAF_URI"]
+			#need to turn it into a string
+			VIAF_URL = str(topic["VIAF_URI"])
+			#for some reason, the RDFXML from VIAF uses http instead of https, so change that:
+			VIAF_URL = VIAF_URL.replace("https", "http")
+			#print(VIAF_URL)
+			#create key-value pair for basket
 			URIdict["basket"] = basketID
+			#create a key for external link, whose value is a dictionary we'll fill in a moment
 			URIdict["external_link"] = {}
+			#create link type key in the external link dictionary, value is text "exactMatch"
 			URIdict["external_link"]["link_type"] = "exactMatch"
+			#since this is VIAF data, external link label is VIAF
 			URIdict["external_link"]["label"] = "VIAF"
+			#create a key for recon data, which is another dictionary that lives in the external link's dictionary
 			URIdict["external_link"]["recon_data"] = {}
 
-# ###NEED to take the URI, grab JSON or XML; grab names and types, create JSON object with basket id, URL+linksource, topic type, and name(s)
-
+			#if there isn't a link, pass		
 			if "None" in VIAF_URL:
 				pass
+			
 			else:
-				URIdict["external_link"]["URL"] = VIAF_URL
-# FAST_URL = "http://id.worldcat.org/fast/1533194"
-# FASTRDFURL = "http://id.worldcat.org/fast/1533194/rdf.xml"
+				#otherwise, the external link is the VIAF URI (the https one)
+				URIdict["external_link"]["URL"] = VIAF_URI
+				print(VIAF_URL)
+				#to get the RDFXML, we use the http VIAF_URL and add the suffix
 				VIAFRDFURL = str(VIAF_URL)+"/rdf.xml"
+				#making an rdflib Graph
 				g = rdflib.Graph()
-
+				#try loading the RDFXML
 				try:
 					g.load(VIAFRDFURL)
-								# for s, p, o in g.triples((None, RDFS.label, None)):
-								# 	#if "type" in p:
+					print(VIAFRDFURL)
+				#if it doesn't work, print the bad VIAF URL (except I'm accidentally passing blank VIAF_URLs here, so it doesn't work)
 				except:
-					print("This VIAF URI had bad data")
-					print(VIAF_URL)
+					print("This VIAF URI had bad data:" + VIAF_URL)
+					print(VIAFRDFURL)
 					pass				# 	print(o)
-
+				#create a list for shortened topic types, because there could be more than one
 				shorttopictypelist = []
+				#look in the graph for the objects in the triple where the subject is the VIAF URI and the predicate is rdf:type
 				for a in g.objects(subject=rdflib.term.URIRef(VIAF_URL), predicate=RDF.type):
 					topictype = a
+					print(topictype)
+					#We are interested in schema.org types
 					if "schema" in topictype:
+						#make it shorter, since we don't want the full URL in our data
 						shorttopictype = topictype.replace("http://schema.org/", "schema:")
+						print(shorttopictype)
+					#sometimes theere's a productontology type though:
 					elif "productontology" in topictype:
 						shorttopictype = topictype.replace("http://www.productontology.org/id/", "pto:")
-					
+						print(shorttopictype)
+					#add all the topic types to a list
 					shorttopictypelist.append(shorttopictype)
 					#print(shorttopictype)
+					#put the list into the dictionary as the key-value for topic type
 					URIdict["external_link"]["recon_data"]["topic_type"] = shorttopictypelist
 
+				#not doing this, but this was how I got VIAF names before
+# 				# viafnameslist = []
 
-				viafnameslist = []
+# 				# for x in g.objects(subject=None, predicate=SKOS.prefLabel):
+# 				# 	viafpreflabel = x
+# 				# 	#print(fastpreflabel)
+# 				# 	viafnameslist.append(viafpreflabel.toPython())
 
-				for x in g.objects(subject=None, predicate=SKOS.prefLabel):
-					viafpreflabel = x
-					#print(fastpreflabel)
-					viafnameslist.append(viafpreflabel.toPython())
-
-				for y in g.objects(subject=None, predicate=SKOS.altLabel):
+# 				# for y in g.objects(subject=None, predicate=SKOS.altLabel):
 					
-					viafaltlabel = y
-					#print(viafaltlabel)
+# 				# 	viafaltlabel = y
+# 				# 	#print(viafaltlabel)
 
-					viafnameslist.append(viafaltlabel.toPython())
+# 				# 	viafnameslist.append(viafaltlabel.toPython())
 
-				for z in g.objects(subject=None, predicate=schema.name):
+# 				# for z in g.objects(subject=None, predicate=schema.name):
 
-					viafname = z
-					#print(viafname)
+# 				# 	viafname = z
+# 				# 	#print(viafname)
 				
-					viafnameslist.append(viafname.toPython())
-				uniqueviafnameslist = f5(viafnameslist)
-				#print(uniqueviafnameslist)
-				URIdict["external_link"]["recon_data"]["topic_hits"] = uniqueviafnameslist
-				#print(URIdict)
+# 				# 	viafnameslist.append(viafname.toPython())
+# 				# uniqueviafnameslist = f5(viafnameslist)
+# 				# #print(uniqueviafnameslist)
+# 				# URIdict["external_link"]["recon_data"]["topic_hits"] = uniqueviafnameslist
+
+				#the URI dict is a record for each topic record to be enriched
+				print(URIdict)
 				URIdictlist.append(URIdict)
 
-# name = g.value(None, SKOS.prefLabel, any=True)
-# print(name)
-# for s, p, o in g:
-# 	print(s)
+# # name = g.value(None, SKOS.prefLabel, any=True)
+# # print(name)
+# # for s, p, o in g:
+# # 	print(s)
 
-
-with open ('VIAFtopicsZRDF_unique_30March.json', 'w') as f:
+#now we'll take the URIdict and make some JSON from it, and write it as a file.
+with open ('VIAF_importantTopics_23March.json', 'w') as f:
 	json.dump(URIdictlist, f, sort_keys=True, ensure_ascii=False, indent=4)
 	print("HOORAY, you did it!")
--- wikidataURIs_enm.py
+++ wikidataURIs_enm.py
@@ -1,40 +1,48 @@
 import requests, json
 from collections import Counter
+from datetime import datetime, date, time
+
+
+filetime = datetime.now()
+filetime = filetime.strftime("%Y-%m-%d_%I-%M_%p")
 
 #getting unique items in a list in a fast way, courtesy of https://www.peterbe.com/plog/uniqifiers-benchmark
 
-def f5(seq, idfun=None): 
-   # order preserving
-   if idfun is None:
-       def idfun(x): return x
-   seen = {}
-   result = []
-   for item in seq:
-       marker = idfun(item)
-       # in old Python versions:
-       # if seen.has_key(marker)
-       # but in new ones:
-       if marker in seen: continue
-       seen[marker] = 1
-       result.append(item)
-   return result
+# def f5(seq, idfun=None): 
+#    # order preserving
+#    if idfun is None:
+#        def idfun(x): return x
+#    seen = {}
+#    result = []
+#    for item in seq:
+#        marker = idfun(item)
+#        # in old Python versions:
+#        # if seen.has_key(marker)
+#        # but in new ones:
+#        if marker in seen: continue
+#        seen[marker] = 1
+#        result.append(item)
+#    return result
 
 URIdictlist = []
 
 
-with open ("Topics_Z.txt") as json_data:
+with open ("important_topics_to_enrich_2017_12_20_03_14_PM_cleaned-tsv_ORexport_2018_03_23_12-18pm.txt") as json_data:
 	topics = json.load(json_data)
 	for rows in topics:
 		
 		for topic in topics[rows]:
 			URIdict = {}
-			basketID = topic['basket - id']
-			Wikidata_URL = str(topic['Wikidata URL'])
-			Wikidata_URL = Wikidata_URL.replace("/wiki", "/entity")
+			basketID = topic["Tct_Id"]
+			Wikidata_URL = str(topic["Wikidata_URI"])
+			#Wikidata_URL = Wikidata_URL.replace("/wiki", "/entity")
 			#print(Wikidata_URL)
 			URIdict["basket"] = basketID
 			URIdict["external_link"] = {}
-			URIdict["external_link"]["link_type"] = "exactMatch"
+			if topic["Wikidata_closeMatch"] == "closeMatch":
+				URIdict["external_link"]["link_type"] = "closeMatch"
+			else:
+				URIdict["external_link"]["link_type"] = "exactMatch"
 			URIdict["external_link"]["label"] = "Wikidata"
 			URIdict["external_link"]["recon_data"] = {}
 			if "None" in Wikidata_URL:
@@ -48,25 +56,25 @@ with open ("Topics_Z.txt") as json_data:
 				WikidataID = Wikidata_URL.replace("https://www.wikidata.org/entity/","")
 
 # 				#print(WikidataID)
-				r = requests.get("https://www.wikidata.org/w/api.php?action=wbgetentities&ids=" + WikidataID + "&format=json")
-				r.encoding = 'utf-8'
-				# "https://www.wikidata.org/w/api.php?action=wbgetentities&ids=Q7330150&format=json"
-				Wikidatadata = json.loads(r.text)
-				#print(Wikidatadata["entities"][WikidataID]["labels"])
-				wikidatanameslist = []
-				names = Wikidatadata["entities"][WikidataID]["labels"]
-				#print(names)
-				for lang in names:
-					langlist.append(lang)
-				for item in langlist:
-					nameobject = Wikidatadata["entities"][WikidataID]["labels"][item]
-					actualname = nameobject["value"]
-					#print(actualname)
-					wikidatanameslist.append(actualname)
+				# r = requests.get("https://www.wikidata.org/w/api.php?action=wbgetentities&ids=" + WikidataID + "&format=json")
+				# r.encoding = 'utf-8'
+				# # "https://www.wikidata.org/w/api.php?action=wbgetentities&ids=Q7330150&format=json"
+				# Wikidatadata = json.loads(r.text)
+				# #print(Wikidatadata["entities"][WikidataID]["labels"])
+				# wikidatanameslist = []
+				# names = Wikidatadata["entities"][WikidataID]["labels"]
+				# #print(names)
+				# for lang in names:
+				# 	langlist.append(lang)
+				# for item in langlist:
+				# 	nameobject = Wikidatadata["entities"][WikidataID]["labels"][item]
+				# 	actualname = nameobject["value"]
+				# 	#print(actualname)
+				# 	wikidatanameslist.append(actualname)
 
-				uniquewikidatanameslist = f5(wikidatanameslist)
+				# uniquewikidatanameslist = f5(wikidatanameslist)
 
-				URIdict["external_link"]["recon_data"]["topic_hits"] = uniquewikidatanameslist
+				# URIdict["external_link"]["recon_data"]["topic_hits"] = uniquewikidatanameslist
 
 				topictypelist = []
 				#Wikidata_testURL = "https://www.wikidata.org/wiki/Q7330150"
@@ -80,7 +88,7 @@ with open ("Topics_Z.txt") as json_data:
 					#print(topictype)
 
 					r = requests.get("https://www.wikidata.org/w/api.php?action=wbgetentities&ids=" + topictype + "&format=json")
-					r.encoding = 'utf-8'
+					r.encoding = "utf-8"
 					Wikidatatopicname = json.loads(r.text)
 					#print(Wikidatatopicname["entities"][topictype]["labels"])
 					topictypename = Wikidatatopicname["entities"][topictype]["labels"]["en"]["value"]
@@ -104,7 +112,7 @@ with open ("Topics_Z.txt") as json_data:
 				#print(URIdict)
 				#URIdictlist.append(URIdict)
 	
-with open ('WikidatatopicsZ_unique_30March.json', 'w') as f:
+with open ("Wikidata_importantTopics_23March.json", 'w') as f:
 	json.dump(URIdictlist, f, sort_keys=True, ensure_ascii=False, indent=4)
 	print("HOORAY, you did it!")
 
